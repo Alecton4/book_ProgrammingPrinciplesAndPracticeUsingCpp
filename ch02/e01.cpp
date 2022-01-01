f4759c84b5bfba2e98fb40d55fe108a084ddd859
az-P1-Win10 <yzhaocl@connect.ust.hk>
2022-01-01 17:35:37 +0800

    Initialization Push to GitHub after finishing ch02

---
+++ ch02/e01.cpp
@@ -0,0 +1,7 @@
+#include "std_lib_facilities.h"
+int main() // C++ programs start by executing the function main
+{
+	cout << "Hello, programming!\nHere we go!\n"; // Output "Hello World!"
+	keep_window_open(); //Wait for a character to be entered
+	return 0;
+}
